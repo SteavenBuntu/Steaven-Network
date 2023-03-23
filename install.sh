1528ddcfd4d487e990b0e139cb3879265f6ff0fa
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-24 01:12:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-sudo rm -rf /etc/netplan/
+sudo rm -rf /etc/netplan
+sudo mkdir /etc/netplan
 sudo wget -O /etc/netplan/01-network-manager-all.yaml https://github.com/SteavenBuntu/Steaven-Network/raw/main/01-network-manager-all.yaml
 sudo netplan apply
