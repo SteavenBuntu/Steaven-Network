5a76597e6cb172f78982403bcf7396b5fb405301
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-24 01:14:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo rm -rf /etc/netplan
 sudo mkdir /etc/netplan
 sudo wget -O /etc/netplan/01-network-manager-all.yaml https://github.com/SteavenBuntu/Steaven-Network/raw/main/01-network-manager-all.yaml
+sudo apt install network-manager-gnome -y
 sudo netplan apply
